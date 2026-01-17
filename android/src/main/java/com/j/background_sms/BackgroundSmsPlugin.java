02b2ff92a4ce8275aeaf8c4ba37215d1a0556e71
belal-emad <belalemad057@gmail.com>
2026-01-17 21:43:11 +0200

    edit register

--- android/src/main/java/com/j/background_sms/BackgroundSmsPlugin.java
+++ android/src/main/java/com/j/background_sms/BackgroundSmsPlugin.java
@@ -1,48 +1,25 @@
 package com.j.background_sms;
 
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.os.Build;
 import android.telephony.SmsManager;
 
 import androidx.annotation.NonNull;
-
-import java.util.UUID;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** BackgroundSmsPlugin */
 public class BackgroundSmsPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+
   private MethodChannel channel;
 
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "background_sms");
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    channel = new MethodChannel(binding.getBinaryMessenger(), "background_sms");
     channel.setMethodCallHandler(this);
-  }
-
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "background_sms");
-    channel.setMethodCallHandler(new BackgroundSmsPlugin());
   }
 
   @Override
@@ -52,22 +29,18 @@ public class BackgroundSmsPlugin implements FlutterPlugin, MethodCallHandler {
       String msg = call.argument("msg");
       Integer simSlot = call.argument("simSlot");
       sendSMS(num, msg, simSlot, result);
-    }else if(call.method.equals("isSupportMultiSim")) {
+    } else if (call.method.equals("isSupportMultiSim")) {
       isSupportCustomSim(result);
-    } else{
+    } else {
       result.notImplemented();
     }
   }
 
-  private void isSupportCustomSim(Result result){
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-      result.success(true);
-    }else{
-      result.success(false);
-    }
+  private void isSupportCustomSim(Result result) {
+    result.success(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O);
   }
 
-  private void sendSMS(String num, String msg, Integer simSlot,Result result) {
+  private void sendSMS(String num, String msg, Integer simSlot, Result result) {
     try {
       SmsManager smsManager;
       if (simSlot == null) {
@@ -89,6 +62,9 @@ public class BackgroundSmsPlugin implements FlutterPlugin, MethodCallHandler {
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
+    if (channel != null) {
+      channel.setMethodCallHandler(null);
+      channel = null;
+    }
   }
 }
